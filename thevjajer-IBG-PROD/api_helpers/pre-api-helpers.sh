d382bec4bb4faf9102d587a263b18228f80b22c5
santudata99 <santudata9@gmail.com>
2023-05-30 02:18:02 -0700

    dynamodb-testing

--- thevjajer-IBG-PROD/api_helpers/pre-api-helpers.sh
+++ thevjajer-IBG-PROD/api_helpers/pre-api-helpers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Executing Pre-API Helpers"
-echo "santu VPC_EC2 DynamoDB testing for DevOps team"
-python ./$CUSTOMIZATION/api_helpers/python/vpc_ec2_cloud5data.py &
+#echo "santu VPC_EC2 DynamoDB testing for DevOps team"
+#python ./$CUSTOMIZATION/api_helpers/python/vpc_ec2_cloud5data.py &
 
-echo "santu DynamoDB testing for DevOps team"
-python ./$CUSTOMIZATION/api_helpers/python/dynamodb_cloud5data.py &
+echo "santu DynamoDB testing for DevSecOps team"
+python ./$CUSTOMIZATION/api_helpers/python/dynamo_cloud5data.py
