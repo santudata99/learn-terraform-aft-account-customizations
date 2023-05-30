76bbb4697dd19888d7a0e2df8909916961527e00
santudata99 <santudata9@gmail.com>
2023-05-30 01:04:57 -0700

    ec2-vpc-dynamodb

--- thevjajer-IBG-PROD/api_helpers/python/dynamodb_cloud5data.py
+++ thevjajer-IBG-PROD/api_helpers/python/dynamodb_cloud5data.py
@@ -1,38 +1,34 @@
-class cloud5data_devops:
-    """Encapsulates an Amazon DynamoDB table of movie data."""
-    def __init__(self, dyn_resource):
-        """
-        :param dyn_resource: A Boto3 DynamoDB resource.
-        """
-        self.dyn_resource = dyn_resource
-        self.table = None
+import boto3
 
-    def create_table(self, table_name):
-        """
-        Creates an Amazon DynamoDB table that can be used to store movie data.
-        The table uses the release year of the movie as the partition key and the
-        title as the sort key.
+dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 
-        :param table_name: The name of the table to create.
-        :return: The newly created table.
-        """
-        try:
-            self.table = self.dyn_resource.create_table(
-                TableName=table_name,
-                KeySchema=[
-                    {'AttributeName': 'year', 'KeyType': 'HASH'},  # Partition key
-                    {'AttributeName': 'title', 'KeyType': 'RANGE'}  # Sort key
-                ],
-                AttributeDefinitions=[
-                    {'AttributeName': 'year', 'AttributeType': 'N'},
-                    {'AttributeName': 'title', 'AttributeType': 'S'}
-                ],
-                ProvisionedThroughput={'ReadCapacityUnits': 10, 'WriteCapacityUnits': 10})
-            self.table.wait_until_exists()
-        except ClientError as err:
-            logger.error(
-                "Couldn't create table %s. Here's why: %s: %s", table_name,
-                err.response['Error']['Code'], err.response['Error']['Message'])
-            raise
-        else:
-            return self.table
+table = dynamodb.create_table(
+    TableName='aftdynamo_cloud5data',
+    KeySchema=[
+        {
+            'AttributeName': 'year',
+            'KeyType': 'HASH'  #Partition_key
+        },
+        {
+            'AttributeName': 'title',
+            'KeyType': 'RANGE'  #Sort_key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'year',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'title',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+
+print("Table status:", table.table_status)
