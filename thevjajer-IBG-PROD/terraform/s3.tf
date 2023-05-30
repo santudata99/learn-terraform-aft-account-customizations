042766ecfddc9b9dce94025e65b0f1ded2a4a936
santudata99 <santudata9@gmail.com>
2023-05-30 00:10:32 -0700

    Dynamo DB

--- thevjajer-IBG-PROD/terraform/s3.tf
+++ thevjajer-IBG-PROD/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "thevjajer-IBG-PROD_bucket" {
-  bucket = "aft-thevjajer-IBG-PROD-${data.aws_caller_identity.current.account_id}"
+resource "aws_s3_bucket" "thevjajer-ibg-prod_bucket" {
+  bucket = "aft-thevjajer-ibg-prod-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
